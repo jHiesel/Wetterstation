4ade44d9848450cbd6b9c51c0ea4f375998f3af7
jHiesel <116503877+jHiesel@users.noreply.github.com>
2023-03-21 09:19:45 +0100

    Some fixes
    Fixed the MySQL database and the Links for the CSS

--- views/layouts/top.php
+++ views/layouts/top.php
@@ -7,11 +7,11 @@
     <link rel="shortcut icon" href="css/favicon.ico" type="image/x-icon">
     <link rel="icon" href="css/favicon.ico" type="image/x-icon">
 
-    <link href="/php42/css/bootstrap.min.css" rel="stylesheet">
-    <link href="/php42/css/index.css" rel="stylesheet">
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/index.css" rel="stylesheet">
 
-    <script src="/php42/js/jquery.min.js"></script>
-    <script src="/php42/js/bootstrap.min.js"></script>
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.js"></script>
 
     <!--  TODO Einbinden von chart.js -->
     <!--  TODO Einbinden von index.js -->
